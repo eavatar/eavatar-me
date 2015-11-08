d0a9226f882e6a0d45caa486a0fb60865aac8bb4
sampot <sam@eavatar.com>
2015-11-08 18:28:02 +0800

    Add more information for classifiers in setup.py
    Although this project is not intended to be uploaded to PyPI, the piece of information is still useful.

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, print_function
 
+import os
+import codecs
 from setuptools import setup, find_packages
-
 from ava import APP_NAME, __version__
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding="utf-8") as fp:
+        return fp.read()
+
 
 setup(
     name=APP_NAME,
     version=__version__,
     description="An event-driven agent for task automation.",
+    long_description=read("README.rst"),
     package_dir={'': 'src'},
-    packages=find_packages(exclude=['**/tests/*']),
-    include_package_data=False,
+    packages=find_packages('src', exclude=['**/tests/**']),
+    include_package_data=True,
 
     zip_safe=False,
     entry_points={
@@ -22,6 +31,19 @@ setup(
     },
 
     author="EAvatar Technology Ltd.",
-    url="http://www.EAvatar.com",
+    url="http://EAvatar.ME",
     license='Apache 2.0',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Environment :: MacOS X :: Cocoa",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications :: GTK",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python"
+    ]
 )
